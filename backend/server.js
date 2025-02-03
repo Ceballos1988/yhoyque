69778f62c1ee3f3b705cc359a63d85ad03203e87
Ceballos1988 <maria.ceballos@davinci.edu.ar>
2025-02-03 17:44:59 -0300

    Cambiando puerto a 8080 para deployment en Railway

--- backend/server.js
+++ backend/server.js
@@ -78,7 +78,7 @@ app.use((req, res, next) => {
 });
 
 // Configuración del puerto en el que correrá el servidor
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
 });
