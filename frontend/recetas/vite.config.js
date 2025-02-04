63783585150b8a7371ce7ce63c96e992f2a9d46b
Ceballos1988 <maria.ceballos@davinci.edu.ar>
2025-02-04 13:22:02 -0300

    Fix: Agregado index.html al repositorio y ajustes en .gitignore

--- frontend/recetas/vite.config.js
+++ frontend/recetas/vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Asegura que las rutas sean correctas en producción
   server: {
     proxy: {
-      '/api': {
-        target: 'https://yhoyque-producción.up.railway.app',
+      "/api": {
+        target: "https://yhoyque-producción.up.railway.app",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
   build: {
     outDir: "dist",
+    manifest: true, // **Asegura que se genere el manifest.json**
     rollupOptions: {
       input: "src/main.jsx",
     },
