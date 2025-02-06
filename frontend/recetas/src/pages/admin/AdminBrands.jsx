3bf2ea42d694e208e9a3ae8c64a63ad36ff12213
Ceballos1988 <maria.ceballos@davinci.edu.ar>
2025-02-06 17:04:57 -0300

    Fix: Spinner

--- frontend/recetas/src/pages/admin/AdminBrands.jsx
+++ frontend/recetas/src/pages/admin/AdminBrands.jsx
@@ -236,7 +236,7 @@ const AdminBrands = () => {
             id="file-upload"
             className="hidden"
           />
-          <button type="submit" className="submit-button">
+          <button type="submit" className="submit-button text-base px-4 py-2 rounded-md font-raleway font-bold transition-all duration-300 bg-[#EE8532] hover:bg-[#0f172b]  text-white">
             Agregar +
           </button>
         </div>
